f254b0c1e766a71d36a03e4939874a6fc2cfbbcb
SungKyum Kim <sungkyum1@naver.com>
2024-03-28 16:47:08 +0900

    feat: DB 정보로 체스보드를 생성하는 기능 구현

--- src/main/java/dao/ChessBoardDAO.java
+++ src/main/java/dao/ChessBoardDAO.java
@@ -1,2 +1,80 @@
-package dao;public class ChessBoardDAO {
+package dao;
+
+import domain.*;
+import domain.piece.Piece;
+import domain.piece.jumping.King;
+import domain.piece.jumping.Knight;
+import domain.piece.pawn.BlackPawn;
+import domain.piece.pawn.WhitePawn;
+import domain.piece.sliding.Bishop;
+import domain.piece.sliding.Queen;
+import domain.piece.sliding.Rook;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class ChessBoardDAO {
+
+    private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
+    private static final String DATABASE = "chess"; // MySQL DATABASE 이름
+    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USERNAME = "root"; //  MySQL 서버 아이디
+    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
+
+    public Connection getConnection() {
+        // 드라이버 연결
+        try {
+            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+        } catch (final SQLException e) {
+            System.err.println("DB 연결 오류:" + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ChessBoard findAll() {
+        final var query = "SELECT * FROM chessboard";
+        try (final var connection = getConnection();
+             final var preparedStatement = connection.prepareStatement(query)) {
+            final ResultSet resultSet = preparedStatement.executeQuery();
+
+            final Map<Square, Piece> pieces = new HashMap<>();
+
+            while (resultSet.next()) {
+                final File file = File.from(resultSet.getString("file"));
+                final Rank rank = Rank.from(Integer.parseInt(resultSet.getString("rank")));
+                final Square square = new Square(file, rank);
+
+                final String pieceName = resultSet.getString("piece");
+                final Piece piece;
+                final Team team = Team.from(resultSet.getString("team"));
+                if (pieceName.equals("king")) {
+                    piece = new King(team);
+                } else if (pieceName.equals("queen")) {
+                    piece = new Queen(team);
+                } else if (pieceName.equals("rook")) {
+                    piece = new Rook(team);
+                } else if (pieceName.equals("bishop")) {
+                    piece = new Bishop(team);
+                } else if (pieceName.equals("knight")) {
+                    piece = new Knight(team);
+                } else {
+                    if (team == Team.BLACK) {
+                        piece = new BlackPawn();
+                    } else {
+                        piece = new WhitePawn();
+                    }
+                }
+                pieces.put(square, piece);
+            }
+
+            return new ChessBoard(pieces);
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/domain/Team.java
+++ src/main/java/domain/Team.java
@@ -9,4 +9,13 @@ public enum Team {
         }
         return WHITE;
     }
+
+    public static Team from(final String name) {
+        for (final Team value : values()) {
+            if (value.name().equalsIgnoreCase(name)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 진영입니다.");
+    }
 }
