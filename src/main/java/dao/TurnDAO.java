f1c9e27764c62478df341f78f9916622002a8e90
SungKyum Kim <sungkyum1@naver.com>
2024-03-31 00:45:39 +0900

    refactor: Connection 생성 객체 분리로 TurnDAO 테스트 시 DB 유지 기능 적용

--- src/main/java/dao/TurnDAO.java
+++ src/main/java/dao/TurnDAO.java
@@ -3,31 +3,23 @@ package dao;
 import domain.Team;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TurnDAO {
-    private static final String SERVER = "localhost:13306";
-    private static final String DATABASE = "chess";
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private final Connection connection;
 
-    public Connection getConnection() {
-        try {
-            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
-        } catch (final SQLException e) {
-            System.err.println("DB 연결 오류:" + e.getMessage());
-            e.printStackTrace();
-            return null;
-        }
+    public TurnDAO() {
+        connection = ConnectionGenerator.getConnection();
+    }
+
+    public TurnDAO(final Connection connection) {
+        this.connection = connection;
     }
 
     public Team find() {
         final var query = "SELECT team FROM turn";
-        try (final var connection = getConnection();
-             final var preparedStatement = connection.prepareStatement(query)) {
+        try (final var preparedStatement = connection.prepareStatement(query)) {
             final ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
                 final String team = resultSet.getString("team");
@@ -41,8 +33,7 @@ public class TurnDAO {
 
     public void update(final Team team) {
         final var query = "update turn set team = ?";
-        try (final var connection = getConnection();
-             final var preparedStatement = connection.prepareStatement(query)) {
+        try (final var preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, team.name().toLowerCase());
 
             preparedStatement.executeUpdate();
--- src/test/java/dao/TurnDAOTest.java
+++ src/test/java/dao/TurnDAOTest.java
@@ -2,30 +2,39 @@ package dao;
 
 import domain.Team;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 class TurnDAOTest {
-    @DisplayName("DB에 저장된 턴을 가져온다.")
-    @Test
-    void turn() {
-        // given
-        final TurnDAO turnDAO = new TurnDAO();
-        // when
-        final Team team = turnDAO.find();
-        //then
-        Assertions.assertThat(team).isEqualTo(Team.WHITE);
+    Connection connection;
+    TurnDAO turnDAO;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        connection = ConnectionGenerator.getConnection();
+        connection.setAutoCommit(false);
+
+        turnDAO = new TurnDAO(connection);
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        connection.rollback();
+        connection.close();
     }
 
     @DisplayName("턴 진행상황을 업데이트 한다.")
     @Test
     void updateTurn() {
-        // given
-        final TurnDAO turnDAO = new TurnDAO();
         // when
-        turnDAO.update(Team.WHITE);
+        turnDAO.update(Team.BLACK);
         final Team team = turnDAO.find();
         //then
-        Assertions.assertThat(team).isEqualTo(Team.WHITE);
+        Assertions.assertThat(team).isEqualTo(Team.BLACK);
     }
 }
